fad1294cce121b0e5ba9d1deedd25f743ee10c37
Lucas Duarte <luckasdu686@gmail.com>
2024-07-31 08:44:55 -0300

    APLICANDO VERIFICAÇÃO DE PATENTES

--- PAGES/home.js
+++ PAGES/home.js
@@ -18,12 +18,13 @@ const firebaseConfig = {
   const db=getFirestore();
 
   const image = document.getElementById('img-user');
+
+  
   
 
   onAuthStateChanged(auth, (user)=>{
     const loggedInUserId=localStorage.getItem('loggedInUserId');
     if(loggedInUserId){
-        console.log(user);
         const docRef = doc(db, "users", loggedInUserId);
         getDoc(docRef)
         .then((docSnap)=>{
@@ -31,15 +32,86 @@ const firebaseConfig = {
                 const userData=docSnap.data();
                 document.getElementById('nome').innerText=userData.nome;
                 document.getElementById('local-escala').innerText=userData.setor;
-                document.getElementById('patente').innerText=userData.nome_patente;
                 document.getElementById('l-pontuacao').innerText=userData.pontos;
-                
-                // foto 
+
+                // Obtendo XP
+                const xp = userData.xp;
+
+                let nome_patente = "";
+
+                // controle de patentes
+                if(xp >= 45 && xp <= 74){
+                    nome_patente = "RECRUTA ATARI"
+
+                }else if(xp >= 75 && xp <= 104){
+                    nome_patente = "SOLDADO ODYSSEY"
+
+                }else if(xp >= 105 && xp <= 144){
+                    nome_patente = "CABO ARCADIA"
+
+                }else if(xp >= 145 && xp <= 184){
+                    nome_patente = "3º SARGENTO"
+
+                }else if(xp >= 185 && xp <= 234){
+                    nome_patente = "2º SARGENTO"
+
+                }else if(xp >= 235 && xp <= 294){
+                    nome_patente = "1º SARGENTO DYNAVISION"
+
+                }else if(xp >= 295 && xp <= 399){
+                    nome_patente = "SUBOFICIAL BOY"
+
+                }else if(xp >= 400 & xp <= 519){
+                    nome_patente = "ASPIRANTE MEGA"
+
+                }else if(xp >= 520 && xp <= 654){
+                    nome_patente = "2º TENENTE TURBO"
+
+                }else if(xp >= 655 && xp <= 804){
+                    nome_patente = "1º TENENTE NEO"
+
+                }else if(xp >= 805 && xp <= 969){
+                    nome_patente = "CAPITÃO SUPER"
+
+                }else if(xp >= 970 && xp <= 1149){
+                    nome_patente = "MAJOR COMMODORE"
+
+                }else if(xp >= 1150 && xp <= 1344){
+                    nome_patente = "TENENTE CORONEL SATURN"
+
+                }else if(xp >= 1345 && xp <= 1554){
+                    nome_patente = "CORONEL PLAY"
+
+                }else if(xp >= 1555 && xp <= 1779){
+                    nome_patente = "BRIGADEIRO 64"
+
+                }else if(xp >= 1780 && xp <= 2019){
+                    nome_patente = "MAJOR BRIGADEIRO ADVANCE"
+
+                }else if(xp >= 2020 && xp <= 2274){
+                    nome_patente = "TENENTE BRIGADEIRO BOX"
+
+                }else if(xp >= 2275 && xp <= 2544){
+                    nome_patente = "MARECHAL 360"
+
+                }else if(xp >= 2545 && xp <= 2829){
+                    nome_patente = "ALMIRANTE ONE"
+
+                }else if(xp >= 2830 && xp <= 3129){
+                    nome_patente = "MESTRE SWITCH"
+
+                }else if(xp >= 3130){
+                    nome_patente = "RAMBO PS5"
+
+                }
+                document.getElementById('patente').innerText= nome_patente
+
+                // foto USER
                 image.src = `https://github.com/LuckasDuarte/KPI_ONLINE/blob/main/FOTOS_COLABORADORES/${userData.nome}.jpg?raw=true`
 
             }
             else{
-                console.log("no document found matching id")
+                console.log("ID DE USUÁRIO NAO CONSTA")
             }
         })
         .catch((error)=>{
@@ -53,6 +125,7 @@ const firebaseConfig = {
 
   const logoutButton=document.getElementById('logout');
 
+  // inserir função de logout 
   logoutButton.addEventListener('click',()=>{
     localStorage.removeItem('loggedInUserId');
     signOut(auth)
